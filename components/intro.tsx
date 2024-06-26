d95eb1a5dcde280e6a124257f1d56edf3545a8c2
jefthah <jefta.supraja@gmail.com>
2024-06-26 21:10:16 +0700

    add intro

---
+++ components/intro.tsx
@@ -0,0 +1,24 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function intro() {
+  return (
+    <section>
+        <div className='flex items-center justify-center'>
+            <div>
+                <Image src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
+                alt='Jefta Supraja'
+                width="192"
+                height="192"
+                quality="95"
+                priority={true}
+                className='h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl'
+                />
+                <span className='absolute text-4xl'>
+                    😊
+                </span>
+            </div>
+        </div>
+    </section>
+  )
+}
