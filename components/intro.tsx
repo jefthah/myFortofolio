ce1bc16d0eb10cfde208cc8e24a1df7012a75aae
jefthah <jefta.supraja@gmail.com>
2024-06-28 11:51:19 +0700

    finishing intro section

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,24 +1,96 @@
-import Image from 'next/image'
-import React from 'react'
+"use client";
 
-export default function intro() {
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+
+export default function Intro() {
   return (
-    <section>
-        <div className='flex items-center justify-center'>
-            <div>
-                <Image src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?fit=crop&w=368&h=368&q=100"
-                alt='Jefta Supraja'
-                width="192"
-                height="192"
-                quality="95"
-                priority={true}
-                className='h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl'
-                />
-                <span className='absolute text-4xl'>
-                    😊
-                </span>
-            </div>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src="/myProfile.jpg" // Path yang benar untuk gambar di folder public
+              alt="Jefta Supraja"
+              width="256" // Lebar diperbesar
+              height="256" // Tinggi diperbesar
+              quality="95"
+              priority={true}
+              className="h-32 w-32 rounded-full object-cover border-[0.35rem] border-white shadow-xl" // Kelas ukuran diperbesar
+            />
+          </motion.div>
+          <motion.span
+            className="absolute bottom-0 right-0 text-4xl"
+            initial={{ opacity: 0, scale: 1 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            😊
+          </motion.span>
         </div>
+      </div>
+
+      <motion.p
+        className="mb-10 mt-4 text-2xl font-medium leading-[1.5] s,:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Jefta Supraja.</span> I am a{" "}
+        <span className="font-bold">front-end developer</span> and currently a{" "}
+        <span className="font-bold">student</span>. I enjoy building{" "}
+        <span className="italic">websites & applications</span>. My focus is on{" "}
+        <span className="underline">React (Next.js)</span>.
+      </motion.p>
+
+      <motion.div
+        className="flex flex-col sm:flex-row justify-center gap-2 px-2 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact Me Here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+        <a className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10" href="/CV.pdf" download>
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/jefta-supraja-925805286/"
+          className="bg-white text-gray-700 px-7 py-3 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/jefthah"
+          className="bg-white text-gray-700 px-7 py-3 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
-  )
+  );
 }
